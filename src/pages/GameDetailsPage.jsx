d0f909f0d133dd43fdcaf225499c82e625b5fda4
unknown <imadesafsafi@gmail.com>
2026-05-05 12:44:27 +0100

    test push

--- src/pages/GameDetailsPage.jsx
+++ src/pages/GameDetailsPage.jsx
@@ -1,5 +1,32 @@
+import axios from "axios";
+import {useParams} from "react-router-dom"
+import { useEffect ,useState } from "react";
+
+
+
 function GameDetailsPage() {
-  return <div></div>;
+  const [game ,setGeme] = useState()
+  const {id} =useParams()
+
+  useEffect(() =>{
+async function fetchGeme(){
+
+  const response = await axios.get(
+   `https://api.rawg.io/api/games/${id}/`
+  )
+  setGeme(response.data)
+  console.log(response);
+  
+}
+fetchGeme()
+} , [id])
+
+ if(!game){
+  return( <h3>Loading...</h3>)
+ }
+ return (<div>
+  <h1>{game.name}</h1>
+  </div>)
 }
 
 export default GameDetailsPage;
