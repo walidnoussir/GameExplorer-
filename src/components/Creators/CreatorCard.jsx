5f518b5a5794a8cb5ccd4f384554429f13f4dd2a
outeglaout jaouad <outeglaoutjaouad@gmail.com>
2026-05-05 14:54:58 +0200

    Fix some errors in useRetreive custom hook & Creating the creators page

--- src/components/Creators/CreatorCard.jsx
+++ src/components/Creators/CreatorCard.jsx
@@ -1,5 +1,15 @@
-const CreatorCard = () => {
-  return <div>CreatorCard</div>;
+import Text from "../Text";
+
+const CreatorCard = ({ id, name, image }) => {
+  return (
+    <div
+      key={id}
+      className="bg-[#1E293B] rounded-2xl flex flex-col gap-2 h-80 justify-center items-center"
+    >
+      <img className="rounded-full size-50" src={image} alt="creator image" />
+      <Text text={name} style={"font-extrabold text-white"} />
+    </div>
+  );
 };
 
 export default CreatorCard;
--- src/components/Creators/CreatorList.jsx
+++ src/components/Creators/CreatorList.jsx
@@ -1,5 +1,26 @@
+import CreatorCard from "./CreatorCard";
+import useRetreive from "../../hooks/useRetreive.js";
 function CreatorList() {
-  return <div>CreatorList</div>;
+  const baseUrl = import.meta.env.VITE_BASE_URL;
+  const secretKey = import.meta.env.VITE_SECRET_KEY;
+  const apiKey = baseUrl + "creators?" + secretKey;
+
+  const { loading, error, data } = useRetreive(apiKey);
+
+  if (loading) {
+    return <div>Loading</div>;
+  } else if (error) {
+    return <div>{error.message}</div>;
+  } else {
+    console.log("Data", data.results);
+    return (
+      <div className="grid md:grid-cols-4 gap-2.5 m-2 sm:grid-cols-3">
+        {data.results.map((creator) => (
+          <CreatorCard {...creator} />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default CreatorList;
